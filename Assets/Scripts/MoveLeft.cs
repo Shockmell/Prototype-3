12fe7f969e049af36ad682e057c86333ad27ab87
Abhinav Bajaj <47352951+Shockmell@users.noreply.github.com>
2020-12-11 16:22:12 +0530

    Main Commit
    Project uploaded

---
+++ Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    private float speed = 30.0f;
+    private PlayerController script;
+    private float leftBound = -15.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        script = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!script.gameOver)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+        if(transform.position.x < leftBound && gameObject.tag == "Obstacle")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    private bool isOnGround = true;
+    private Animator anim;
+    private AudioSource playerAudio;
+    
+    public bool gameOver = false;
+    public float jumpForce = 10.0f;
+    public float gravModifier = 1.0f;
+    public ParticleSystem explosion;
+    public ParticleSystem dirt;
+    public AudioClip jump;
+    public AudioClip crash;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravModifier;
+        anim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            dirt.Stop();
+            playerAudio.PlayOneShot(jump, 1.0f);
+            isOnGround = false;
+            anim.SetTrigger("Jump_trig");
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            dirt.Play();
+            isOnGround = true;
+        }
+        else if(collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            explosion.Play();
+            playerAudio.PlayOneShot(crash, 1.0f);
+            dirt.Stop();
+            anim.SetBool("Death_b", true);
+            anim.SetInteger("DeathType_int", 1);
+        }
+    }
+}
